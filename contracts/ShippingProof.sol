1e2fe5378f6ff2f878f78196fabfab206b5abd9c
jsd <jsd@users.noreply.github.com>
2020-10-02 13:03:04 -0400

    Update ShippingProof.sol

--- contracts/ShippingProof.sol
+++ contracts/ShippingProof.sol
@@ -10,12 +10,6 @@ contract ShippingProof is ChainlinkClient {
     bytes32 private jobId;
     uint256 private fee;
 
-    /**
-     * Network: Ropsten
-     * Oracle: Chainlink - 0xc99B3D447826532722E41bc36e644ba3479E4365
-     * Job ID: Chainlink - 3cff0a3524694ff8834bda9cf9c779a1
-     * Fee: 0.1 LINK
-     */
     constructor(address _link) public {
         if(_link == address(0)) {
           setPublicChainlinkToken();
@@ -24,12 +18,15 @@ contract ShippingProof is ChainlinkClient {
         }
         oracle = 0x0bE828811DB2bf0Eb0d28F0a697B5514A7cc70B7;
         jobId = "f08e3ca2aba94c6aafcd610843d3d4d9";
-        fee =  1 * LINK;  // 0.1 LINK
+        fee =  1 * LINK;  
     }
 
-    function requestShippingStatus(address _oracle, string memory _jobId) public returns (bytes32 requestId) {
+    /**
+     * Receive the stautus of the delivery 
+     */
+    function requestShippingStatus(address _oracle, string memory _jobId, string memory _trackingNbr) public returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), address(this), this.fulfill.selector);
-        req.add("id", "300403513353");
+        req.add("id", _trackingNbr);
         req.add("copyPath", "status");
         return sendChainlinkRequestTo(_oracle, req, fee);
     }
